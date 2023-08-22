14c1733269e069fa9c6156fc4d855df1563a32ff
Marcelo Lima <marcelo.lima4@aluno.senai.br>
2023-08-22 08:33:20 -0300

    feat: init project

---
+++ assets/js/script.index.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(name, email, date, city, telephone, cpf ){
+        this.name = name;
+        this.email = email;
+        this.date = date;
+        this.city = city;
+        this.telephone = telephone;
+        this.cpf = cpf;
+    }
+}
